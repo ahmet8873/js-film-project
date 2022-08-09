01ba2f19533a1b79ecc952daece049acdd55e546
Ahmet Avci <ahmetavci.8873@gmail.com>
2022-08-09 16:28:00 +0200

    localden de sildik

--- project.js
+++ project.js
@@ -2,6 +2,7 @@ const form=document.getElementById("film-form");
 const titleElement=document.querySelector("#title");
 const directorElement=document.querySelector("#director");
 const urlElement=document.querySelector("#url");
+const cardBody=document.querySelectorAll(".card-body")[1];
 
 const ui=new UI();
 const storage=new Storage();
@@ -13,6 +14,7 @@ function eventListeners(){
         let films=storage.getFilmsFromStorage();
         ui.loadAllFilms(films);
     })
+    cardBody.addEventListener("click",deleteFilm);
 
 }
 
@@ -35,4 +37,13 @@ function  addFilm(e){
     ui.clearInputs(titleElement,directorElement,urlElement);
     
     e.preventDefault();
+}
+
+function deleteFilm(e){
+    if(e.target.id="delete-film"){
+        ui.deleteFilmFromUI(e.target);
+        storage.deleteFilmFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+        ui.displayMassages("silme islemi basarili..","success");
+        
+    }
 }
--- ui.js
+++ ui.js
@@ -42,3 +42,6 @@ UI.prototype.loadAllFilms=function(films){
   })
 
 }
+UI.prototype.deleteFilmFromUI=function(element){
+  element.parentElement.parentElement.remove();
+}
